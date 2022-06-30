1e04b54908ea2d436dfcf4f43988fa35cd7d022b
madchavez <chavez.neo@gmail.com>
2022-06-30 21:54:14 +0800

    Revert "Revert "Added LoginTab and used it as the form at startup.""
    This reverts commit d6d8ec0dc317882bc4668d0e649b79a4de9fe45a.

---
+++ LibraryManagementSystem/Forms/LoginTab.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LibraryManagementSystem.Forms
+{
+    public partial class LoginTab : System.Windows.Forms.Form
+    {
+        public LoginTab()
+        {
+            InitializeComponent();
+        }
+        
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source = LAPTOP-ID3FFBBJ\\SQLEXPRESS; Initial Catalog = libraryData; Integrated Security = True");
+            con.Open();
+            cmd = new SqlCommand("SELECT * FROM loginData WHERE username = '" + txtUser.Text + "' AND password ='" + txtPass.Text + "'", con);
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login Success", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+                this.Hide();
+                Home newTab = new Home();
+                newTab.ShowDialog();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Error", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+
+}
--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -16,7 +16,7 @@ namespace LibraryManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new Forms.LoginTab());
         }
     }
 }
