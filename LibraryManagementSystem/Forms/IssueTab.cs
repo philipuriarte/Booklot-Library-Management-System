a94b6160803b5a2e40dd9578a1038682dac1a9ff
madchavez <chavez.neo@gmail.com>
2022-07-07 15:00:22 +0800

    Added message boxes

--- LibraryManagementSystem/Forms/IssueTab.cs
+++ LibraryManagementSystem/Forms/IssueTab.cs
@@ -20,7 +20,8 @@ namespace LibraryManagementSystem.Forms
         SqlConnection con;
         SqlCommand cmd;
         SqlDataAdapter da;
-
+        DataTable dt;
+        int bookID;
         private void btnBack_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -37,10 +38,10 @@ namespace LibraryManagementSystem.Forms
 
         private void btnIssue_Click(object sender, EventArgs e)
         {
-            con = new SqlConnection("Data Source=DESKTOP-9MBNT14\\SQLEXPRESS;Initial Catalog=libraryData;Integrated Security=True");
+            con = new SqlConnection("Data Source=LAPTOP-ID3FFBBJ\\SQLEXPRESS;Initial Catalog=libraryData;Integrated Security=True");
             con.Open();
 
-            int bookID = Convert.ToInt32(txtBookID.Text);
+            /*int bookID = Convert.ToInt32(txtBookID.Text);
             string libraryID = txtLibraryID.Text;
             string unavailText = "Borrowed by student library ID: " + libraryID;
 
@@ -52,7 +53,43 @@ namespace LibraryManagementSystem.Forms
             con.Close();
 
             txtBookID.Clear();
-            txtLibraryID.Clear();
+            txtLibraryID.Clear();*/
+
+            string libraryID = txtLibraryID.Text;
+            string unavailText = "Borrowed by student library ID: " + libraryID;
+
+
+            if (String.IsNullOrEmpty(txtBookID.Text) || String.IsNullOrEmpty(txtLibraryID.Text))
+            {
+                
+                
+                MessageBox.Show("Please enter Book ID and Library ID.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else {
+                bookID = Convert.ToInt32(txtBookID.Text);
+                cmd = new SqlCommand("SELECT * FROM booksData WHERE BookID = '" + bookID + "' AND Status = 'avail'", con);
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    cmd = new SqlCommand("UPDATE booksData SET Status = '" + unavailText + "' WHERE BookID = '" + bookID + "'", con);
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Successfully issued book ID " + bookID + " to student " + libraryID, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    con.Close();
+
+                    txtBookID.Clear();
+                    txtLibraryID.Clear();
+
+                }
+                else {
+                    MessageBox.Show("Invalid or unavailable book.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtBookID.Clear();
+                    txtLibraryID.Clear();
+                }
+            }
         }
     }
 }
