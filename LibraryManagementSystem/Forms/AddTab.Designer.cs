7ff37c6cf9089dffa7cf96ad90e125531113bcda
PhilipU13 <philipuriarte@gmail.com>
2022-07-08 01:01:23 +0800

    Added auto-complete functionality to cmbGenre if text is entered.

--- LibraryManagementSystem/Forms/AddTab.Designer.cs
+++ LibraryManagementSystem/Forms/AddTab.Designer.cs
@@ -97,6 +97,8 @@
             // cmbGenre
             // 
             this.cmbGenre.AllowDrop = true;
+            this.cmbGenre.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cmbGenre.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.cmbGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbGenre.ForeColor = System.Drawing.Color.Black;
             this.cmbGenre.FormattingEnabled = true;
