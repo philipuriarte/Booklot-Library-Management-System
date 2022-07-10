7b01eab2931dd4e87fbfd80e95a9ed76d2e56622
PhilipU13 <philipuriarte@gmail.com>
2022-07-10 18:22:48 +0800

    Add code to btnDelete in "DeleteMemberTab". It can now delete members from the database.

--- LibraryManagementSystem/Forms/DeleteMemberTab.Designer.cs
+++ LibraryManagementSystem/Forms/DeleteMemberTab.Designer.cs
@@ -110,6 +110,7 @@
             this.btnDelete.TabIndex = 3;
             this.btnDelete.Text = "DELETE";
             this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // label3
             // 
