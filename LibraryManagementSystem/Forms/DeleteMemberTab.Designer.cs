f9e7699f0bd8473b80d103bf121e39cbb1feb8b1
PhilipU13 <philipuriarte@gmail.com>
2022-07-10 17:48:12 +0800

    Create DeleteMemberTab form.

---
+++ LibraryManagementSystem/Forms/DeleteMemberTab.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace LibraryManagementSystem.Forms
+{
+    partial class DeleteMemberTab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeleteMemberTab));
+            this.SuspendLayout();
+            // 
+            // DeleteMemberTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DeleteMemberTab";
+            this.Text = "Booklot Library Management System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
